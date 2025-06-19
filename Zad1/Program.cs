b920cddcb39d7d732f2e348bf6f280245b0124d3
Desislava <MVPdesixristova@gmail.com>
2025-06-19 10:41:36 +0300

    Add files via upload

---
+++ Zad1/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace Zad1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+                Dictionary<string, int> letterCounts = new Dictionary<string, int>();
+                string name;
+                while ((name = Console.ReadLine()) != "stop")
+                {
+                    if (letterCounts.ContainsKey(name))
+                    {
+                        letterCounts[name]++;
+                    }
+                    else
+                    {
+                        letterCounts.Add(name, 1);
+                    }
+                }
+                foreach (var entry in letterCounts)
+                {
+                    Console.WriteLine($"{entry.Key} -> {entry.Value}");
+                }
+            }
+        }
+    }
+    
+
---
+++ zad2.19.06/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace zad2._19._06
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, List<string>> studentBooks = new Dictionary<string, List<string>>();
+            List<string> studentOrder = new List<string>();
+            string line;
+            while ((line = Console.ReadLine()) != "end")
+            {
+                string[] parts = line.Split(new string[] { " -> " }, StringSplitOptions.None);
+                string studentName = parts[0];
+                string bookTitle = parts[1];
+                if (!studentBooks.ContainsKey(studentName))
+                {
+                    studentBooks.Add(studentName, new List<string>());
+                    studentOrder.Add(studentName);
+                }
+                    studentBooks[studentName].Add(bookTitle);
+            }
+            foreach (string studentName in studentOrder)
+            {
+                Console.WriteLine($"{studentName}: {string.Join(", ", studentBooks[studentName])}");
+            }
+
+        }
+
+        }
+    }
